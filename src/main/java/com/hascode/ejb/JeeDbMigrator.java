0e4de97fb348898efb47ed7307ba13e76fa4d942
Micha Kops <micha.kops@gmail.com>
2014-10-07 18:14:03 +0200

    Maven coordinates, JNDI resource lookup fixed.

--- src/main/java/com/hascode/ejb/JeeDbMigrator.java
+++ src/main/java/com/hascode/ejb/JeeDbMigrator.java
@@ -17,23 +17,21 @@ import com.googlecode.flyway.core.api.MigrationInfo;
 public class JeeDbMigrator {
 	private final Logger log = Logger.getLogger(getClass().getName());
 
-	@Resource(name = "jdbc/__default")
+	@Resource(lookup = "jdbc/__default")
 	private DataSource dataSource;
 
 	@PostConstruct
 	private void onStartup() {
 		if (dataSource == null) {
 			log.severe("no datasource found to execute the db migrations!");
-			throw new EJBException(
-					"no datasource found to execute the db migrations!");
+			throw new EJBException("no datasource found to execute the db migrations!");
 		}
 
 		Flyway flyway = new Flyway();
 		flyway.setInitOnMigrate(true);
 		flyway.setDataSource(dataSource);
 		for (MigrationInfo i : flyway.info().all()) {
-			log.info("migrate task: " + i.getVersion() + " : "
-					+ i.getDescription() + " from file: " + i.getScript());
+			log.info("migrate task: " + i.getVersion() + " : " + i.getDescription() + " from file: " + i.getScript());
 		}
 		flyway.migrate();
 	}
